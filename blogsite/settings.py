ed4e98933d3b370aaf21949c6625a030ad17f8d3
Asher <haarisharry007@gmail.com>
2023-02-14 15:10:38 +0530

    added a csrf agent

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -129,7 +129,7 @@ MEDIA_URL = '/uploads/'
 LOGIN_URL = "/login"
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://web-production-571a.up.railway.app/'
+    'https://web-production-571a.up.railway.app'
 ]
 
 
